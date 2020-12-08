dbef86183c5a2819c3c6ab5eafa5a964d93efdcc
gylych207 <mammedov.207@gmail.com>
2020-12-08 15:22:44 -0500

    search added

--- src/components/Tutors.jsx
+++ src/components/Tutors.jsx
@@ -9,42 +9,52 @@ import { useHistory } from "react-router-dom";
 const Tutors = (props) => {
   let { id } = useParams();
   const [tutor, setTutor] = useState("");
-  // api call for a single item
-  // useEffect(() => {
-  //   let getData = async () => {
-  //     let response = await axios.get(`${baseURL}/${id}`, config);
-  //     setTutor(response.data);
-  //     console.log('tutor',response.data);
-  //   };
-  //   getData();
-  // }, []);
+  const [search, setSearch] = useState("");
+
+  function searchInput(e) {
+    setSearch(e.target.value);
+    let filterResult = props.data.filter((tutor) =>
+      tutor.fields.lessons.toLowerCase().includes(search)
+    );
+    console.log(filterResult);
+  }
 
   return (
     <div className="tutorsPage">
       <div className="wallpaper">
-        <input type="text" placeholder="Search" className="search" />
+        <input
+          type="text"
+          placeholder="Search"
+          className="search"
+          value={search}
+          onChange={searchInput}
+        />
       </div>
       <div className="tutorsContainer">
         {props.data &&
-          props.data.map((tutor) => {
-            return (
-              <div className="tutorContainer">
-               <Link to="/TutorInfo"><p className="name" onClick={(e) => {
-                  if (e.target.textContent === tutor.fields.name) {
-                    props.setTutorInfo(tutor.fields);
-                    
-                  
-                  }
-                
-                  
-                }}>{tutor.fields.name}</p></Link>
+          props.data
+            .filter((tutor) => tutor.fields.lessons.toLowerCase().includes(search))
+            .map((tutor) => {
+              return (
+                <div className="tutorContainer">
+                  <Link to="/TutorInfo">
+                    <p
+                      className="name"
+                      onClick={(e) => {
+                        if (e.target.textContent === tutor.fields.name) {
+                          props.setTutorInfo(tutor.fields);
+                        }
+                      }}
+                    >
+                      {tutor.fields.name}
+                    </p>
+                  </Link>
 
-                <img src={tutor.fields.img} alt="" className="image"></img>
-                <p className="price">$ {tutor.fields.price}</p>
-               
-              </div>
-            );
-          })}
+                  <img src={tutor.fields.img} alt="" className="image"></img>
+                  <p className="price">$ {tutor.fields.price}</p>
+                </div>
+              );
+            })}
       </div>
     </div>
   );
