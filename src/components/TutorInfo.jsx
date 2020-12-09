f3299ab15c33fca5c407bfa228f9d268afe85bb4
gylych207 <mammedov.207@gmail.com>
2020-12-08 19:34:20 -0500

    styled subject info

--- src/components/TutorInfo.jsx
+++ src/components/TutorInfo.jsx
@@ -18,13 +18,13 @@ const TutorInfo = (props) => {
         <p>{props.tutorInfo.about}</p>
         <div class="button_cont" align="center"><Link to='/Payment'><span class="example_c" target="_blank" rel="nofollow noopener">BOOK</span></Link></div>
         <div className="otherInfo">
-          <p className="subject">Subject: {props.tutorInfo.lessons}</p>
-          <p className="price">Price: {props.tutorInfo.price}</p>
-          <p className="session">Session Duration: {props.tutorInfo.session}</p>
-         
-          <p className='addSession' onClick={() => setNumber(number + 1)}>Add Session: {number}</p>
-          <p className='addSession' onClick={() => number >= 1 ? setNumber(number - 1): 0}>Remove Session</p>
-          <p className='totalPrice'>Total Price:{parseInt(number)*parseInt(props.tutorInfo.price)}</p>
+          <p className="tutoringInfo">Subject: {props.tutorInfo.lessons}</p>
+          <p className="tutoringInfo">Price: {props.tutorInfo.price}$</p>
+          <p className="tutoringInfo">Session Duration: {props.tutorInfo.session}</p>
+          <p className="tutoringInfo">You have {number} session</p>
+          <button className='addSession' onClick={() => setNumber(number + 1)}>Add Session:</button>
+          <button className='removeSession' onClick={() => number >= 1 ? setNumber(number - 1): 0}>Remove Session:</button>
+          <p className='tutoringInfo'>Total Price:{parseInt(number)*parseInt(props.tutorInfo.price)}</p>
 
         </div>
       </div>
